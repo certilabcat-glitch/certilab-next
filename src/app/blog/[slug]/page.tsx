8cdd268442ebc003ca8a6492a46501872ee0f953
ABDELAZIZ <paramedicashop@gmail.com>
2026-06-01 12:35:40 +0200

    Fix: quita 'use client' de blog/page.tsx y blog/[slug]/page.tsx - rompia build en Vercel

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import { articles, getArticle, getRelatedArticles } from "@/data/articles";
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,5 @@
-"use client";
-
 import Link from "next/link";
+import type { Metadata } from "next";
 import { articles } from "@/data/articles";
 import "./blog.css";
 
